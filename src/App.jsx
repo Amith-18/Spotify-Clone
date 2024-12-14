cbb17cc85e2a6930174ddd213e63ebd0047e08b2
Amith-18 <amithsanil@outlook.com>
2024-12-14 22:48:02 +0400

    Added home,search,library in sidebar.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import Sidebar from './Components/Sidebar'
+
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
-      
+      <div className='flex-grow'></div>
+      <Sidebar/>
     </div>
   )
 }
 
 export default App
+
+
