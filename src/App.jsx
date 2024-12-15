0b0e570bce99472a372ba9e15d90affab1a72155
Amith-18 <amithsanil@outlook.com>
2024-12-15 21:35:33 +0400

    added new file player.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Sidebar from './Components/Sidebar'
+import Player from './Components/Player'
 
 
 const App = () => {
   return (
     <div className='h-screen bg-black'>
-      <div className='flex-grow'></div>
-      <Sidebar/>
+      <div className='h-[90%] flex'>
+        <Sidebar />
+      </div>
+      <Player />
     </div>
   )
 }
